ceaebaa2c245b87618fe767baf08fcbf66d01894
poejark <josephpark842@gmail.com>
2025-02-24 20:51:36 +0000

    input event listener added to photo url input

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
+'use strict';
 const $image = document.querySelector('#photo-url');
 if (!$image) throw new Error('image element does not exist');
-
-$image.addEventListener('input', (event: Event) => {
-  const $eventTarget = event.target as HTMLInputElement;
+$image.addEventListener('input', (event) => {
+  const $eventTarget = event.target;
   if (!$eventTarget) throw new Error('no input value detected');
   $image.setAttribute('src', $eventTarget.value);
   console.log($eventTarget.value);
